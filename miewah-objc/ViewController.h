1e00aed63b33869d700af7199020bb47b883272b
Monsoir <monwingyeung@gmail.com>
2018-04-19 15:38:22 +0800

    What: 1.创建工程 2.使用 Carthage 引入 ReactiveCocoa

---
+++ miewah-objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  miewah-objc
+//
+//  Created by Christopher on 2018/4/19.
+//  Copyright © 2018 wenyongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ miewah-objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  miewah-objc
+//
+//  Created by Christopher on 2018/4/19.
+//  Copyright © 2018 wenyongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
