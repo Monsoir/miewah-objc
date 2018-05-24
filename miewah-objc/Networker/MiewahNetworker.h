4e1ea104c0941b56fa878df6b8fb9aa0f15feef2
Monsoir <monwingyeung@gmail.com>
2018-05-24 21:52:23 +0800

    What: 1.调整结构 2.添加部分注释

--- miewah-objc/Networker/MiewahNetworker.h
+++ miewah-objc/Networker/MiewahNetworker.h
@@ -5,6 +5,8 @@
 //  Created by Christopher on 2018/4/28.
 //  Copyright © 2018 wenyongyang. All rights reserved.
 //
+//  GET 请求使用的是一个单例对象
+//
 
 #import <Foundation/Foundation.h>
 #import "MiewahRequestConvention.h"
--- miewah-objc/Networker/MiewahPostNetworker.h
+++ miewah-objc/Networker/MiewahPostNetworker.h
@@ -5,6 +5,8 @@
 //  Created by Christopher on 2018/5/24.
 //  Copyright © 2018 wenyongyang. All rights reserved.
 //
+//  每个 POST 请求使用一个单独的对象，尤其是要设置头部的请求
+//
 
 #import <Foundation/Foundation.h>
 #import "MiewahRequestConvention.h"
